c13f37abf593d705eacd5c32765378a28a589c90
zxc <379417284@qq.com>
2024-04-18 07:05:20 +0800

    first commit

---
+++ mydecoder.cpp
@@ -0,0 +1,6 @@
+#include "mydecoder.h"
+
+myDecoder::myDecoder()
+{
+
+}
---
+++ mydecoder.h
@@ -0,0 +1,11 @@
+#ifndef MYDECODER_H
+#define MYDECODER_H
+
+
+class myDecoder
+{
+public:
+    myDecoder();
+};
+
+#endif // MYDECODER_H
